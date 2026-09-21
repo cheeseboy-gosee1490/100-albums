feff546120ad4ac53f1b1afc1fb9592416c24673
cheeseboy-gosee1490 <83649561+cheeseboy-gosee1490@users.noreply.github.com>
2026-09-21 12:21:16 +0100

    Refresh artwork fix on iPhone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="100-albums-v3";
+const CACHE="100-albums-v4";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./manifest.webmanifest"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
